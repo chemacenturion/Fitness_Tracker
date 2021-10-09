a6a7e8a8028105c7509c1ae909638a53d3cea6ff
chemacenturion <centurion.philadelphia@gmail.com>
2021-10-08 22:54:27 -0400

    added config/connection

---
+++ config/connection.js
@@ -0,0 +1,13 @@
+const { connect, connection } = require('mongoose');
+
+connect(process.env.MONGODB_URI || 'mongodb://localhost/workout',
+{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+}
+
+);
+
+module.exports = connection;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const connection = require('./config/connection');
 
 const PORT = process.env.PORT || 3000;
 
@@ -7,15 +8,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
-
-// routes
 app.use(require("./routes"));
 
 app.listen(PORT, () => {
