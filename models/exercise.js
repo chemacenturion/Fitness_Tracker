4387d276dd050b2f82e097dba2344b5d4dcdeae6
chemacenturion <centurion.philadelphia@gmail.com>
2021-10-08 23:04:16 -0400

    pushing up exerciseSchema

---
+++ models/exercise.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require('mongoose');
+
+const exerciseSchema = new Schema({
+    name: {
+        type: String,
+    },
+    type:  {
+        type: String,
+    },
+    weight: {
+        type: Number,
+    },
+    reps: {
+        type: Number,
+    },
+    sets: {
+        type: Number,
+    },
+    duration: {
+        type: Number,
+    },
+    distance_traveled: {
+        type: Number,
+    },
+});
+
+
+
+module.exports = exerciseSchema;
